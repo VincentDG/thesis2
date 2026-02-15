ae234f6bf53b9b2e1e2c8837d2542ec40cb6fe77
BerryAmazing <45611489+BerryAmazing@users.noreply.github.com>
2026-02-15 21:32:04 +0800

    parsing for traces and events
    added code makes arrays called traces and events
    
    traces contains the contents of each trace
    events contain the contents of each event in each trace

--- parser.py
+++ parser.py
@@ -1,6 +1,7 @@
 import gzip
 import shutil
 import os
+import re
 
 # This section of the code deals with relative file paths
 dirname = os.path.dirname(__file__)
@@ -15,5 +16,31 @@ with gzip.open(rel_path, 'rb') as f_in:
 
 # This section of the code opens the dataset
 with open(rel_path[:-3], 'rb') as dataset:
-    print(dataset.read())
-    
+    contents = dataset.read()
+contents = str(contents)
+
+# This section of the code looks for trace logs and saves the contents of each trace into an array
+traces = []
+i = 0
+while re.search("<trace>", contents):
+    x = re.search("<trace>", contents)
+    y = re.search("</trace>", contents)
+    start = x.start()
+    i = y.end()
+    traces.append(contents[start:i])
+    contents = contents[i:]
+
+# This section of the code looks for events in each trace log and saves the contents of each event into an array of arrays
+events = []
+for trace in traces:
+    i = 0
+    event_log = []
+    while re.search("<event>", trace):
+        x = re.search("<event>", trace)
+        y = re.search("</event>", trace)
+        start = x.start()
+        i = y.end()
+        event_log.append(trace[start:i])
+        trace = trace[i:]
+    events.append(event_log)
+print(events[0])
